0ca07311b831bf1d0db2abb10a59680be4155edc
Oscar Björkgren <osbjorkg@abo.fi>
2019-03-08 17:16:07 +0200

    added gitignore and started working on tests

--- experiment-fittslaw/src/main/java/hci/experiment/fittslaw/core/GameData.java
+++ experiment-fittslaw/src/main/java/hci/experiment/fittslaw/core/GameData.java
@@ -5,10 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-/**
- *
- * @author Lenovo
- */
 public class GameData {
     // Metadata
     public final String id;
---
+++ experiment-fittslaw/src/test/java/UnitTests/Tests.java
@@ -0,0 +1,15 @@
+package UnitTests;
+
+import junit.framework.*;
+
+public class Tests extends TestCase{
+    
+    protected void setUp(){
+        
+    }
+    // Verify experiment configuration parameters
+    public void experimentConfiguration_Test(){
+        assertTrue(true);
+    }
+    
+}
